92edf94bc6b561611e3b300df717aff3656f78be
Mattias Fucik <107649652+AdamWallgren28@users.noreply.github.com>
2024-04-09 23:09:54 +0200

    completes compnents lektion3 & lektion4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 // import { UserProfile } from './components/lektion3';
 
 // Lektion 4
-import { TraditionalGreeting, ArrowGreeting, UserProfile4 } from './components/lektion4';
+import { TraditionalGreeting, ArrowGreeting, UserProfile4, Card, Counter2, TitleUpdater, ToDoList } from './components/lektion4';
 
 
 
@@ -29,9 +29,25 @@ function App() {
       </div> */}
 
       {/*Lektion 4*/}
+      
       <TraditionalGreeting name="Adam"/>
       <ArrowGreeting name="Adam" />
+      <hr/>
       <UserProfile4 name="Adam" age="28" location="Seattle"/>
+      <hr/>
+      <Card>
+        <h3>Plock </h3>
+        <h2>Warrior G6.1 Pro</h2>
+        <img src="https://www.warrioreurope.com/pub/media/catalog/product/cache/image/8706e245c2991d92f8e0b03c5f265359/g/6/g61ppt2www_war_01_i_1.png" style={{ transform: 'rotate(-90deg)', width: '15vw'}} />
+        <h1>4 499 kr</h1>
+      </Card>
+      <hr/>
+      <Counter2 />
+      <hr/>
+      <TitleUpdater />
+      <hr/>
+      <ToDoList />
+      <hr/>
     </div>
   );
 }
--- src/components/lektion4.jsx
+++ src/components/lektion4.jsx
@@ -1,23 +1,121 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect, useReducer } from 'react';
 
+// Uppgift 1
 export function TraditionalGreeting(props){
-    return (        
-        <h2> Hej hej, {props.name}!!</h2> 
-    );  
+    return (
+            <p><em>Hej hej, {props.name}!!</em></p>
+    );
 }
 
-export let ArrowGreeting = (props) =>    
-        <h2> Hej hej igen, {props.name}!!</h2>;
+export let ArrowGreeting = (props) =>
+<p><em> Hej hej igen, {props.name}!!</em></p>;
+        
 
 
-export function UserProfile4({name, age, location}){
+// Uppgift 2
+export function UserProfile4({name='anonym', age='okänd', location='gömd'}){
     return (
         <>
-            <hr/>
-            <h1>Användare: {name}</h1>
+            <h2>Användare: {name}</h2>
             <p>Ålder: {age}</p>
             <p>Plats: {location}</p>
         </>
-        
     );
+}
+
+// Uppgift 3
+export function Card({children}){
+    return (
+        <div class="produkt">
+            {children}
+        </div>
+    );
+}
+
+// Uppgift 4
+export function Counter2() {
+    let [count, setCount] = useState(0);
+
+    function eatCookie () {
+        if (count > 0) {
+            setCount(count - 1);
+        }
+    }
+
+  return (
+    <div>
+      <h2>Cookie maker</h2>
+      <p>You have {count} cookies.</p>
+      <button onClick={() => setCount(count + 1)}>Make a Cookie!</button>
+      <button onClick={eatCookie}>Eat a Cookie!</button>
+    </div>
+  );
+  }
+
+  // Uppgift 5
+export function TitleUpdater() {
+    let [titleCount, setTitleCount] = useState(0);
+
+    function updateTitleCount() {
+        setTitleCount(titleCount +1);
+    }
+
+    useEffect(() => {
+        if (titleCount > 0){
+            document.title = `New Title nr ${titleCount}`;
+        }
+    });
+    
+    return (
+        <button onClick={updateTitleCount}>Set new title!</button>
+    )
+}
+
+// Uppgift 6
+let ADD_ITEM = 'ADD_ITEM';
+let REMOVE_ITEM = 'REMOVE_ITEM';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case ADD_ITEM:
+            return [...state, action.payload];
+        case REMOVE_ITEM:
+            return state.filter((_, index) => index !== action.payload);
+        default:
+            return state;
+    }
+};
+
+export function ToDoList() {
+    const [items, dispatch] = useReducer(reducer, []);
+
+    function addItem(){
+        const newItemContent = document.getElementById('newItem').value;
+        if (newItemContent.trim() !== '') {
+            dispatch({ type: ADD_ITEM, payload: newItemContent });
+            document.getElementById('newItem').value = '';
+        }
+    }
+
+    function removeItem(index) {
+        dispatch({ type: REMOVE_ITEM, payload: index });
+    }
+
+    return (
+        <>
+            <h2>Att-göra lista</h2>
+            <input type="text" id="newItem" />
+            <button onClick={addItem}>Add item</button>
+            <ul>
+            {items.map((item, index) => (
+                    <li key={index}>
+                        {item} <button onClick={() => removeItem(index)}>Remove item</button>
+                    </li>
+                ))}
+            </ul>
+
+            <h3>useReducer vs. useState</h3>
+            <p>useReduce är onödigt komplicerad för en så enkel sak som en "att gör"-lista. Säkert bra till mer komplexa grejer.</p>
+        </>
+    )
 }
